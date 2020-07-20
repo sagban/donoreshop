43eed85d91f2bbaa6c815e77ce2b14309e5b33df
dhirajdgandhi <soniaa.godhwani@gmail.com>
2020-07-20 02:01:07 -0400

    Donor App models

--- donoreshop_backend/donorApp/admin.py
+++ donoreshop_backend/donorApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from donorApp.models import Donor, CartProductRelation, Cart, Wallet
+
+admin.site.register(Wallet)
+admin.site.register(Donor)
+admin.site.register(Cart)
+admin.site.register(CartProductRelation)
--- donoreshop_backend/donorApp/models.py
+++ donoreshop_backend/donorApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Wallet(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    amount = models.fields.FloatField(blank=True)
+    amountSpent = models.fields.FloatField(blank=True)
+
+
+class Donor(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    name = models.fields.TextField(blank=True)
+    email = models.fields.EmailField(blank=True)
+    phone = models.fields.TextField(blank=True)
+    age = models.fields.IntegerField(blank=True, null=True)
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
+    # event = models.ForeignKey(Event)
+    amountDonated = models.fields.FloatField(blank=True)
+    amountUsed = models.fields.FloatField(blank=True)
+
+
+class CartProductRelation(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    # productDonated = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # productRequired = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.fields.IntegerField(blank=True)
+    perProduct = models.fields.FloatField(blank=True)
+
+
+

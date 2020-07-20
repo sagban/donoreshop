aa8b485f77d4baa848b52fff5adb7ebaa96cbec9
dhirajdgandhi <soniaa.godhwani@gmail.com>
2020-07-20 02:49:19 -0400

    URLS for Donor and Ngo

--- donoreshop_backend/ngoApp/urls.py
+++ donoreshop_backend/ngoApp/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from ngoApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('donor/', include('donorApp.urls')),
-    path('ngo/', include('ngoApp.urls'))
+    path('home/', views.home, name='home'),
 ]
